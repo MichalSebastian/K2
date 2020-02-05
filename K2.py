43ee0663c5642e19f60c8b3e39563f4838852a33
MichalSebastian <60648281+MichalSebastian@users.noreply.github.com>
2020-02-05 13:50:25 +0100

    Add files via upload
    Added SQLite, current weather data is now stored in db (either in file or in memory - one is # out). Single table is used.
    print_db() function to print out avg/min/max temperature from database.
    Changed time for datetime for handling for getting current time as timestamp for primary key.
    Substituted sleep() from time library with plt.pause() (from matplotlib) - killed to birds with one stone - handles the refresh time and fixes issue when plot was not generated until main loop end.
    Now includes main().

--- K2.py
+++ K2.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import requests #for fetching data via HTTP
 import matplotlib.pyplot as plt #for plotting
-import time #for time related functions
+from datetime import datetime #for time related functions
+import sqlite3 #use SQLite
+from sqlite3 import Error
 
 def get_weather(datastr):
     url = "http://api.openweathermap.org/data/2.5/weather"
@@ -17,8 +19,7 @@ def plot_forecast(forecast):
     x=[forecast['list'][i]['dt_txt'] for i in range(40)] #x axis get time data
     y1=[forecast['list'][i]['main']['temp'] for i in range(40)] #y1 axis get temperature data
     y2=[forecast['list'][i]['wind']['speed'] for i in range(40)] #y2 axis get wind speed data
-    
-    fig, ax1 = plt.subplots(figsize=(15,5))
+    fig, ax1 = plt.subplots(figsize=(15,5)) #defining the plot size
     ax2=ax1.twinx() #include two Y axis on single plot and single X axis
     ax1.plot(x,y1, 'bo-') #plot temperature in blue
     ax2.plot(x,y2, 'y-') #plot wind speed in yellow
@@ -27,10 +28,9 @@ def plot_forecast(forecast):
     ax1.grid(axis="both") #display grid on plot
     ax1.set_xticklabels(x, rotation=90) #rotate the time labels for visibility
     plt.title("5-day weather forecast for "+forecast['city']['name']) #setting the plot label, uses fetched data to display city name
-    plt.pause(0.0001)  #fix for not plotting while in loop
     print("\n") #endline after plot
     
-def display_weather(weather):
+def display_weather(weather): #display current weather data
     print("Current weather:")
     print("Conditions:",weather['weather'][0]['description'])
     print("Temperature:",weather['main']['temp'],"°C")
@@ -40,29 +40,77 @@ def display_weather(weather):
     print("Wind direction:",weather['wind']['deg'],"°")
     print("\n") #endline after displaying data
 
-k2 = {"lat": "35.88", "lon": "76.51"} #define coordinates
-key = {"APPID": "8aadfa69e450f31dad65406b2ba9eb34"} #define OpenWeather API key
-unit = {"units": "metric"} #define units, default=metric
-datastr = {**k2, **key, **unit} #create data string for url request
-sleep_time = 5 #time
+def print_db(wheatherdb): #display avg, min, max temperature from database
+    c = wheatherdb.cursor()
+    c.execute("SELECT AVG(temperature) FROM data")
+    temps=c.fetchall()
+    print('Average temperature: ',temps)
+    c.execute("SELECT MIN(temperature) FROM data")
+    temps=c.fetchall()
+    print('Lowest temperature: ',temps)
+    c.execute("SELECT MAX(temperature) FROM data")
+    temps=c.fetchall()
+    print('Highest temperature: ',temps)
 
-while True:
-    weather = get_weather(datastr) #fetch weather data
-    forecast = get_forecast(datastr) #fetch forecast data
-    
-    if weather['cod'] in [200]: #chceck if wheather was correctly updated
-        display_weather(weather)
-    elif weather['cod'] in [429]: #chceck if wheather has hit refresh limit
-        print("Error: Too many inquiries (weather).")
-    else: #if any other error occured while fetching weather data
-       print("An error occured while fetching weather.")
-    
-    if forecast['cod'] in ['200']: #chceck if forecast was correctly updated
-        plot_forecast(forecast)
-    elif forecast['cod'] in ['429']: #chceck if wheather has hit refresh limit
-        print("Error: Too many inquiries (forecast).")
-    else: #if any other error occured while fetching forecast data
-        print("An error occured while fetching forecast.")
-    
-    time.sleep(sleep_time) #wait for a defined amount of seconds
-    
+def create_connection():
+    wheatherdb = None 
+    try:
+        wheatherdb = sqlite3.connect(':memory:') #create a database in RAM
+        #wheatherdb = sqlite3.connect(r".\weather_file1.db") #create a database on disk (current folder)
+    except Error as e:
+        print(e) #print error if error occurs
+    if wheatherdb is not None:
+        try:
+            c = wheatherdb.cursor()
+            c.execute(""" CREATE TABLE IF NOT EXISTS data (update_time text PRIMARY KEY, temperature int, pressure int, humidity int, wind_speed int, wind_direction int, clouds int, conditions text); """) #create a table to store weather data
+        except Error as e:
+            print(e) #print error if error occurs
+    else:
+        print("Error! cannot create the database connection.")    
+    return wheatherdb #return the connection object
+
+def write_to_db(wheatherdb, weather):
+    updt_tme=datetime.now() #update current time
+    updt_tme=updt_tme.strftime("%d/%m/%Y, %H:%M:%S") #change the date/time format
+    row=( #create row of data to store in database
+        updt_tme, #stores timestamp as primary key for db table
+        weather['main']['temp'],
+        weather['main']['pressure'],
+        weather['main']['humidity'], 
+        weather['wind']['speed'], 
+        weather['wind']['deg'],
+        weather['clouds']['all'],
+        weather['weather'][0]['description'])
+    c = wheatherdb.cursor()
+    c.execute("insert into data values (?,?,?,?,?,?,?,?)", row) #store data in database
+
+def main():
+    wheatherdb=create_connection()
+    k2 = {"lat": "35.88", "lon": "76.51"} #define coordinates
+    key = {"APPID": "8aadfa69e450f31dad65406b2ba9eb34"} #define OpenWeather API key
+    unit = {"units": "metric"} #define units, default=metric
+    datastr = {**k2, **key, **unit} #create data string for url request
+    sleep_time = 3 #time
+    while True:
+        weather = get_weather(datastr) #fetch weather data
+        forecast = get_forecast(datastr) #fetch forecast data
+        if weather['cod'] in [200]: #chceck if wheather was correctly updated
+            print("Data updating every ", sleep_time, " seconds.", "\n")
+            display_weather(weather)
+            write_to_db(wheatherdb,weather)
+            print_db(wheatherdb)
+        elif weather['cod'] in [429]: #chceck if wheather has hit refresh limit
+            print("Error: Too many inquiries (weather).")
+        else: #if any other error occured while fetching weather data
+           print("An error occured while fetching weather.")
+        
+        if forecast['cod'] in ['200']: #chceck if forecast was correctly updated
+            plot_forecast(forecast)
+        elif forecast['cod'] in ['429']: #chceck if wheather has hit refresh limit
+            print("Error: Too many inquiries (forecast).")
+        else: #if any other error occured while fetching forecast data
+            print("An error occured while fetching forecast.")
+        plt.pause(sleep_time)  #wait for a defined amount of seconds + fix for not plotting while in loop while using time.sleep()
+
+if __name__== "__main__":
+  main()    
